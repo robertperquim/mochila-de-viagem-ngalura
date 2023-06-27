9be1ce4318ec34ebf1d2da38b2e1f58e723c26af
Robert Perquim <perquimrobert@gmail.com>
2023-06-27 15:02:02 -0300

    adicioando os items no local storage do navegador para depois ao recarregar a pagina eles permanecerem

--- Js/main.js
+++ Js/main.js
@@ -1,5 +1,6 @@
 const form = document.getElementById("novoItem");
 const lista = document.getElementById("lista");
+const itens = [];
 
 form.addEventListener("submit", (evento) => {
   evento.preventDefault();
@@ -23,4 +24,13 @@ function criaElemento(nome, quantidade) {
   novoItem.innerHTML += nome;
 
   lista.appendChild(novoItem);
+
+  const itemAtual = {
+    nome: nome,
+    quantidade: quantidade,
+  };
+
+  itens.push(itemAtual);
+
+  localStorage.setItem("item", JSON.stringify(itens));
 }
