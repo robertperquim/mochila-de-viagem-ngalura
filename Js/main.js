034584fbc7db1e35ce39f63ce8d064536aa96ad7
Robert Perquim <perquimrobert@gmail.com>
2023-06-27 16:09:55 -0300

    Adicionando a funcionalidade de atualizar a quantidade de um item. Apenas a parte visual - falta implementar a parte de atualziar no localstorage

--- Js/main.js
+++ Js/main.js
@@ -12,16 +12,26 @@ form.addEventListener("submit", (evento) => {
   const nome = evento.target.elements["nome"];
   const quantidade = evento.target.elements["quantidade"];
 
+  const existe = itens.find((elemento) => elemento.nome === nome.value);
+
   const itemAtual = {
     nome: nome.value,
     quantidade: quantidade.value,
   };
 
-  itens.push(itemAtual);
+  if (existe) {
+    itemAtual.id = existe.id;
+
+    atualizaElemento(itemAtual);
+  } else {
+    itemAtual.id = itens.length;
+
+    criaElemento(itemAtual);
+
+    itens.push(itemAtual);
+  }
 
   localStorage.setItem("itens", JSON.stringify(itens));
-
-  criaElemento(itemAtual);
 
   nome.value = " ";
   quantidade.value = " ";
@@ -33,9 +43,14 @@ function criaElemento(item) {
 
   const numeroItem = document.createElement("strong");
   numeroItem.innerHTML = item.quantidade;
-
+  numeroItem.dataset.id = item.id;
   novoItem.appendChild(numeroItem);
   novoItem.innerHTML += item.nome;
 
   lista.appendChild(novoItem);
+}
+
+function atualizaElemento(item) {
+  document.querySelector("[data-id='" + item.id + "']").innerHTML =
+    item.quantidade;
 }
